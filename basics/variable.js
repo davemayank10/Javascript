f833d0993ae636609be7dc1d2f33e88672dbbe42
Mayank Dave <127477037+davemayank10@users.noreply.github.com>
2024-06-25 16:29:55 +0000

    commit

---
+++ basics/variable.js
@@ -0,0 +1,8 @@
+const accountId = 1005
+let accountEmail = "davemayank05@gmail.com"
+let accountPassword = "12345"
+let accountCity = "Vadodara"
+let accountState
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
