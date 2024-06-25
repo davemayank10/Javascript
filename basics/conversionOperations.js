7a475a82c4f6922d922ab9d5d01feb043325b61b
Mayank Dave <127477037+davemayank10@users.noreply.github.com>
2024-06-25 17:14:12 +0000

    operations

---
+++ basics/conversionOperations.js
@@ -0,0 +1,29 @@
+let value = 3
+let negvalue = -value
+//console.log(negvalue)
+
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 / 2)
+// console.log(2 % 2)
+// console.log(2 ** 3)
+
+let str1 = "Hello"
+let str2 = " Mayank"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log( (3 + 2) * 5 % 3)
+
+console.log(+true)
+console.log(+"")
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
