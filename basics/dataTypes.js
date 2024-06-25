9761eb9f31f4b78b8009a9dbf89679f71e96784d
Mayank Dave <127477037+davemayank10@users.noreply.github.com>
2024-06-25 16:48:43 +0000

    commit

---
+++ basics/dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"//treat all js code as newer version
+
+console.log("Mayank")
+
+let name = "Mayank"
+let age = "21"
+let isLoggedin = true
+
+// number
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+
+//object
+
+console.log(typeof name)
+console.log(typeof undefined)
+console.log(typeof null)
