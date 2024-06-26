52d90e88376c20b77d62040316f547e233ccda33
Mayank Dave <127477037+davemayank10@users.noreply.github.com>
2024-06-26 17:20:40 +0000

    dataType_summary

---
+++ basics/dataTypes_summary.js
@@ -0,0 +1,33 @@
+//Primitive
+
+// string , number , boolean , null , undefined , symbol , bigint
+
+const score = 100
+const scorevalue = 100.9
+
+const isLoggedin = true
+const outsideTemp = null
+let userEmail
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const bigint = 132154116546n
+
+//Reference (Non Primitive)
+
+//arrays , objects , functions
+
+const heros = ["shaktiman", "nagraj"] //array
+
+let myObj = {               //object
+    name: "Mayank",
+    age: 21
+}
+
+const myFunction = function () {  
+    console.log("Hello World");   //function
+}
+
